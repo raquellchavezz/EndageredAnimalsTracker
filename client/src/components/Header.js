a905eca9c34f6b2163e074d96afb9565b87ab237
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-22 18:13:23 -0700

    inital set up

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1 className="header"> Animal Sighting tracker </h1>
+        {/* <div className="navbar">
+          <ul>
+            <li><Link to="">Home</Link></li>
+
+            <li><Link to="add">Add Sighting</Link></li>
+          </ul>
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default Header;
