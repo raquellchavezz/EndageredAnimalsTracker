1095e609350214feef0cbf0ecb91b807f35695c8
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-24 00:55:34 -0700

    adds import statement for AddSighting component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import AddSpecies from "./components/AddSpecies";
 import Header from "./components/Header";
+import AddSighting from "./components/AddSightings";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddSpecies />
+      <AddSighting />
     </div>
   );
 }
